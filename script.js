bdfc36063e8928f0b2527d28484420c1e5c13210
nisa-07 <nisatasmia07@gmail.com>
2025-03-20 18:20:54 +0000

    initial commit for my fist web project

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example: Show an alert when the contact form is submitted
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();  // Prevent form submission
+    alert("Form submitted!");
+  });
+  
